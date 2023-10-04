324a86cc61a18b63ffaca924de49107cc4bb50da
rguido <rodrigoguidoarias92@gmail.com>
2023-10-04 21:03:59 +0200

    commit3

--- src/practicaeventos_rguidoarias/PracticaEventos_RGuidoArias.java
+++ src/practicaeventos_rguidoarias/PracticaEventos_RGuidoArias.java
@@ -13,20 +13,20 @@ public class PracticaEventos_RGuidoArias {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-   
-//         Tabla tabla1 = new Tabla();
-//      
-//        tabla1.setVisible(true);
-//        tabla1.setSize(400,500);
-//        tabla1.setLocation(500,400);
-//        tabla1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    Manejador e = new Manejador();
-    e.setVisible(true);
-    e.setSize(500,700);
-    e.setLocation(500,400);
-    e.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         
+        // Tabla tabla1 = new Tabla();
+        //
+        // tabla1.setVisible(true);
+        // tabla1.setSize(400,500);
+        // tabla1.setLocation(500,400);
+        // tabla1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Manejador e = new Manejador();
+        e.setVisible(true);
+        e.setSize(500, 700);
+        e.setLocationRelativeTo(null);
+        e.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 
 }
